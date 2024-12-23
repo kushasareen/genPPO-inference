3c22baf0e35b407cbaec611cbbc1d118ad2b85de
Kusha Sareen <kusha.sareen@cn-f002.server.mila.quebec>
2024-12-23 18:25:28 -0500

    support for PPO RM

--- reward_model.py
+++ reward_model.py
@@ -145,3 +145,96 @@ class LLMAsAJudge(GenVinePPOVerifier):
         feedback = response.outputs[0].text
 
         return score, token, feedback
+
+class PPOVerifier(torch.nn.Module):
+    def __init__(self, args, tokenizer = None):
+        super().__init__()
+        self.tokenizer = tokenizer
+
+        self.prm_tokenizer = AutoTokenizer.from_pretrained(f"{args.reward_model}", cache_dir=args.download_dir)
+        self.prm_model = AutoModelForCausalLM.from_pretrained(f"{args.reward_model}",
+                                                        torch_dtype=torch.float16, cache_dir=args.download_dir).eval()
+        self.prm_model.to(args.device)
+        self.device = args.device
+        self.token_count = 0
+
+        self.tokenizer = tokenizer
+        self.verification_question = args.verification_question
+        self.token_count = 0
+        self.args = args
+
+    async def get_score_from_model(self, question, solution):
+        # To better understand the alignment of inputs, logits, logps, and labels,
+        # we provide a detailed explanation below.
+        # -----------------------------------------------------------------------------------------------------------
+        # Consider the sequence: "<s> q1 q2 a b c </s>", where "<s> q1 q2" forms the prompt, and "a b c </s>"
+        # the response, with `prompt_len = 3` and `response_len = 4`. Additionally, we include a padding token at
+        # the end for the sake of generality.
+        #
+        # Here is the inputs dictionary setup:
+        # Inputs:
+        # [     <s>           q1           q2           a            b            c           </s>         <p>   ]
+        # Attn Mask:
+        # [       1            1            1           1            1            1              1           0   ]
+        # Labels:
+        # [    -100         -100         -100        ID_a         ID_b         ID_c        ID_</s>        -100   ]
+        # >>> seq_len = torch.sum(attn_mask) = 7
+        #
+        # Feeding the Inputs+Attn Mask, the model outputs logits for next tokens in the sequence:
+        # Logits:
+        # [  p(.|<s>)      p(.|q1)      p(.|q2)      p(.|a)       p(.|b)       p(.|c)      p(.|</s>)    p(.|<p>)]
+        #
+        # We exclude the nonsensical last logit (predicting beyond </s>). Also, to obtain the logprobs of next
+        # ground-truth token, we shift the labels by 1 to the left:
+        #
+        # Valid Logits:
+        # [  p(.|<s>)      p(.|q1)      p(.|q2)      p(.|a)       p(.|b)       p(.|c)      p(.|</s>)  ]
+        # Shifted Labels:
+        # [     -100         -100         ID_a        ID_b         ID_c        ID_</s>         -100   ]
+        # Shifted Labels Mask (aka Action Mask), i.e. shifted_labels != -100, highlighting valid token-preds/actions:
+        # [        0            0            1           1            1            1              0   ]
+        # Aligning them to obtain logprobs (lp) of predicting the next token (or lp of action):
+        # [    lp(q1)       lp(q2)        lp(a)       lp(b)        lp(c)      lp(</s>)       lp(<p>)  ]
+        #
+        #
+        # Applying the labels mask gives us logprobs of predicting the valid response tokens (aka actions):
+        # [     -inf          inf         lp(a)       lp(b)        lp(c)      lp(</s>)         -inf   ]
+        # Rewriting the original shifted inputs (for clarity). These are the states we care about:
+        # [      <s>           q1           q2           a            b            c           </s>   ]
+        # In this example, with S=[<s>;q1;q2] as the state and A=a, we compute lp(A|S) = log(p(a|<s>;q1;q2)) = lp(a)
+        #
+        # Note that the values are also computed for the entire sequence, but similar to inputs, we ignore
+        # the last one since it nonsensical (i.e. V(</s>) is not used).
+        # Valid Values:
+        # [    V(<s>)        V(q1)        V(q2)        V(a)         V(b)         V(c)        V(</s>)  ]
+        # Applying the action mask:
+        # [     -inf         -inf         V(q2)        V(a)         V(b)         V(c)          -inf   ]
+        #
+        # >>> logits_seq_len = logps_seq_len = valid_values_len = seq_len - 1 = 6
+
+        input_for_prm = f"{question} {solution}" # this should be fine
+
+        input_id = torch.tensor([self.prm_tokenizer.encode(input_for_prm)]).to(self.device)
+        with torch.no_grad():
+            logits = self.prm_model(input_id) # TODO: inspect shape
+            # scores = logits.softmax(dim=-1)[:,:,0] 
+            # log_prob = scores.log()
+            # step_log_prob = log_prob[input_id == self.step_tag_id]
+            # step_log_prob = step_log_prob.cpu()[-1].item()
+            step_log_prob = logits
+        return step_log_prob
+    
+    async def forward(self, prompt, solutions):
+        tasks = []
+
+        for solution in solutions:
+            tasks.append(asyncio.create_task(self.get_score_from_model(prompt, solution)))
+
+        logprobs = [await task for task in tasks]
+
+        tokens = ['N/A'] * len(solutions)
+        full_feedbacks = ['N/A'] * len(solutions)
+        return logprobs, tokens, full_feedbacks
+
+
+reward_models = {'mathsphere': MathSphereRewardModel, 'genppo': GenVinePPOVerifier, 'llm-as-a-judge': LLMAsAJudge, 'ppo': PPOVerifier}
