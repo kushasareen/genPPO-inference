fabd9b791a2f1ea99f9e14e1305384f79784a4af
Kusha Sareen <kusha.sareen@cn-c032.server.mila.quebec>
2025-02-07 16:44:26 -0500

    update run.sh to ensure job finishes

--- scripts/run.sh
+++ scripts/run.sh
@@ -25,7 +25,7 @@ sbatch <<EOT
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 
 module load anaconda
 cd /home/mila/k/kusha.sareen/genPPO/genPPO-inference
