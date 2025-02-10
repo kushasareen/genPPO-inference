73c406da4d6a574fa69b30a0ce3098ed574cf447
Kusha Sareen <kusha.sareen@cn-e003.server.mila.quebec>
2025-02-10 11:07:38 -0500

    added plots

---
+++ plotting/collect.py
@@ -0,0 +1,161 @@
+import argparse
+import numpy as np
+import os
+import json
+import pandas as pd
+
+def get_data(model):
+    path = f"/home/mila/k/kusha.sareen/scratch/genPPO/outputs/{model}"
+    json_files = os.listdir(path)
+    data = {}
+    for file in json_files:
+        # Check if the file is a JSON file (and not a directory)
+        file_path = os.path.join(path, file)
+        if os.path.isfile(file_path) and not file.lower().endswith('.csv'):
+            with open(file_path, "r") as f:
+                data[file] = json.load(f)
+    return data
+
+def parse_data(data):
+    metrics = {}
+    true_k = {}
+    
+    for key, content in data.items():
+        split = key.split('_')
+        method = split[1]
+        
+        
+        k = int(''.join(filter(str.isdigit, split[3])))
+        seed = int(''.join(filter(str.isdigit, split[4])))
+        adv = content['config']['use_advantage']
+
+        if method != "rebase":
+            bestofn_to_df(content, method, k, adv)
+        
+        for metric, values in content.items():
+            if metric == "config" or metric == "base_results":
+                continue
+            
+            first_key = list(values.keys())[0]
+            if not isinstance(values[first_key], dict):
+                keys = list(values.keys()) # remove all text
+                int_keys = [int(key.split('@')[-1]) for key in keys]
+                max_key = keys[int_keys.index(max(int_keys))]
+                max_key_value = values[max_key]
+                max_int_key = int(max_key.split('@')[-1])
+                if max_int_key == 1 and metric != "total_tokens":
+                    max_key_value = content["base_results"]["top 1"]
+
+
+                key_tuple = (metric, k, adv)
+                if key_tuple not in metrics:
+                    metrics[key_tuple] = []
+                    true_k[key_tuple] = []
+                metrics[key_tuple].append(max_key_value)
+                true_k[key_tuple].append(max_int_key)
+
+            else:  # Metrics with nested sub-metrics
+                for sub_metric, sub_values in values.items():
+                    keys = list(sub_values.keys())
+                    int_keys = [int(key.split('@')[-1]) for key in keys]
+                    max_key = keys[int_keys.index(max(int_keys))]
+                    max_key_value = sub_values[max_key]
+                    max_int_key = int(max_key.split('@')[-1])
+                    if max_int_key == 1 and metric != "total_tokens":
+                        max_key_value = content["base_results"]["top 1"]
+                        
+                    metric_name = f'{metric}_{sub_metric}'
+                    key_tuple = (metric_name, k, adv)
+                    if key_tuple not in metrics:
+                        metrics[key_tuple] = []
+                        true_k[key_tuple] = []
+                    metrics[key_tuple].append(max_key_value)
+                    true_k[key_tuple].append(max_int_key)
+                
+    # Compute averages
+    aggregated_results = {}
+    aggregated_std = {}
+    aggregated_true_k = {}
+    aggregated_true_k_std = {}
+    for key_tuple, values in metrics.items():
+        aggregated_results[key_tuple] = np.mean(values)
+        aggregated_std[key_tuple] = np.std(values)
+        aggregated_true_k[key_tuple] = np.mean(true_k[key_tuple])
+        aggregated_true_k_std[key_tuple] = np.std(true_k[key_tuple])
+    
+    # Convert to DataFrame  
+    df = convert_to_df(aggregated_results, aggregated_true_k, aggregated_std, aggregated_true_k_std)
+    df.to_csv(f"/home/mila/k/kusha.sareen/scratch/genPPO/outputs/{args.model}/rebase_results.csv", index=False)
+
+def bestofn_to_df(content, method, k, adv):
+    assert method == "bestofn"
+    # save content to csv in same format as rebase
+    
+    rows = []
+    for metric, values in content.items():
+        if metric == "config" or metric == "base_results":
+            continue
+        
+        first_key = list(values.keys())[0]
+        if not isinstance(values[first_key], dict):  # Metrics with multiple k-values (like pass_at_k)
+            for key, value in values.items():
+                k_int = int(key.split('@')[-1])
+                rows.append({
+                    'metric': metric,
+                    'k': k_int,
+                    'adv': False,
+                    'value': value,
+                    'true_k': k_int,
+                    'std': 0,
+                    'true_k_std': 0
+                })
+        else:  # Metrics with nested sub-metrics
+            for sub_metric, sub_values in values.items():
+                metric_name = f'{metric}_{sub_metric}'
+                for key, value in sub_values.items():
+                    k_int = int(key.split('@')[-1])
+                    rows.append({
+                        'metric': metric_name,
+                        'k': k_int,
+                        'adv': False,
+                        'value': value,
+                        'true_k': k_int,
+                        'std': 0,
+                        'true_k_std': 0
+                    })
+
+    # Convert the list of rows into a DataFrame
+    df = pd.DataFrame(rows)
+
+    # Optional: Reorder columns for clarity
+    df = df[['metric', 'k', 'adv', 'value', 'true_k', 'std', 'true_k_std']]
+    df.to_csv(f"/home/mila/k/kusha.sareen/scratch/genPPO/outputs/{args.model}/bestofn_results.csv", index=False)
+
+def convert_to_df(data, true_k, std, true_k_std):
+    rows = []
+    for (metric, k, adv), value in data.items():
+        rows.append({
+            'metric': metric,
+            'k': k,
+            'adv': adv,
+            'value': value,
+            'true_k': true_k[(metric, k, adv)],
+            'std': std[(metric, k, adv)],
+            'true_k_std': true_k_std[(metric, k, adv)]
+        })
+
+    # Convert the list of rows into a DataFrame
+    df = pd.DataFrame(rows)
+
+    # Optional: Reorder columns for clarity
+    df = df[['metric', 'k', 'adv', 'value', 'true_k', 'std', 'true_k_std']]
+    return df
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='qwen_genPPO_0.5')
+    args = parser.parse_args()
+
+    data = get_data(args.model)
+    parse_data(data)
---
+++ plotting/plot.py
@@ -0,0 +1,216 @@
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rcParams
+import os
+
+rcParams.update({'font.size': 14})  # Increase base font size for the entire plot
+
+def load_csvs(model_names):
+    data = {}
+    for model in model_names:
+        path = f"/home/mila/k/kusha.sareen/scratch/genPPO/outputs/{model}"
+        csv_files = os.listdir(path)
+        for file in csv_files:
+            # Check if the file is a CSV file (and not a directory)
+            file_path = os.path.join(path, file)
+            if os.path.isfile(file_path) and file.lower().endswith('.csv'):
+                data[(model, file)] = pd.read_csv(file_path)
+
+    return data
+
+def get_linestyles(num_linestyles):
+    assert num_linestyles <= 4, "Only 4 linestyles are supported"
+    return ['solid', 'dashed', 'dotted', 'dashdot'][:num_linestyles]
+
+def get_base_colors(method_names):
+    assert len(method_names) <= 9, "Only 9 base colors are supported"
+
+    colors = [
+        "#1f77b4",  # Blue
+        "#ff7f0e",  # Orange
+        "#2ca02c",  # Green
+        "#d62728",  # Red
+        "#9467bd",  # Purple
+        "#8c564b",  # Brown
+        "#e377c2",  # Pink
+        "#7f7f7f",  # Gray
+        "#bcbd22",  # Olive
+    ]
+
+    return {method: colors[i] for i, method in enumerate(method_names)}
+
+def plot_method(method_data, model, file, fig_maj, fig_bon, fig_wmaj, fig_pan, fig_tok, color_dict, suffix_to_plot):
+    # Extract the metric names
+    search_method = file.split(".")[0].split("_")[0]
+    method_data["suffix"] = method_data["metric"].apply(lambda x: x.split("_")[-1] if "_" in x else "")
+    
+    # Define linestyle mapping
+    linestyle_map = {"sum": "solid", "min": "dashed", "last": "dotted"}
+    
+    # Define figure mapping for different plot types
+    fig_map = {
+        "majority_vote": fig_maj,
+        "best_of_n": fig_bon,
+        "weighted_majority_vote": fig_wmaj,
+        "pass_at_k": fig_pan,
+        "total_tokens": fig_tok,
+    }
+
+    # Get unique base method names
+    base_methods = sorted(set(m.split("_sum")[0].split("_min")[0].split("_last")[0] for m in method_data["metric"]))
+    base_methods.remove("time")
+
+    # Assign colors to base methods
+    for adv in [True, False]:
+        if method_data[method_data["adv"] == adv].empty:
+            continue
+
+        model_and_search_name = "_".join([model, search_method])
+        if adv:
+            model_and_search_name += "_adv"
+
+        color = color_dict[model_and_search_name]
+        method_data_adv = method_data[method_data["adv"] == adv]
+
+        for base_method in base_methods:
+            sub_data = method_data_adv[method_data["metric"].str.startswith(base_method)]
+            
+            # Determine which figure to use
+            key = base_method
+            fig = fig_map.get(key)
+
+            ax = fig.gca()
+
+            if sub_data["suffix"].nunique() == 1:
+                # sort by true_k
+                sub_data = sub_data.sort_values("true_k")
+                ax.errorbar(
+                    sub_data["true_k"],
+                    sub_data["value"],
+                    yerr=sub_data["std"],
+                    label=f"{model_and_search_name}",
+                    linestyle="solid",
+                    color=color,
+                    marker="o",
+                    capsize=3
+                )
+                print(key, model_and_search_name)
+                ax.legend()  # Ensure legend is added if any labels exist
+                ax.set_title(key)
+                ax.set_xlabel("k")
+                ax.set_ylabel("Value")
+                ax.set_xscale("log", base=2)
+                if key == "total_tokens":
+                    ax.set_yscale("log", base=2)
+                ax.grid(True)
+            else:
+                for suffix, linestyle in linestyle_map.items():
+                    if suffix not in suffix_to_plot:
+                        continue
+                    sub_sub_data = sub_data[sub_data["metric"].str.endswith(suffix)]
+                    # sort by true_k
+                    sub_sub_data = sub_sub_data.sort_values("true_k")
+                    ax.errorbar(
+                        sub_sub_data["true_k"],
+                        sub_sub_data["value"],
+                        yerr=sub_sub_data["std"],
+                        label=f"{model_and_search_name} ({suffix})",
+                        linestyle=linestyle,
+                        color=color,
+                        marker="o",
+                        capsize=3
+                    )
+                    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+                    ax.set_title(key)
+                    ax.set_xlabel("k")
+                    ax.set_ylabel("Value")
+                    ax.set_xscale("log", base=2)
+                    ax.grid(True)
+
+    return fig_maj, fig_bon, fig_wmaj, fig_pan, fig_tok
+
+def flatten_list(l):
+    return [item for sublist in l for item in sublist]
+    
+def plot_all(data, model_names, save_path="plots", suffix_to_plot=["sum", "min", "last"]):
+    model_and_search_names  = [[f"{model}_rebase", f"{model}_rebase_adv", f"{model}_bestofn"] for model in model_names]
+    model_and_search_names = flatten_list(model_and_search_names)
+
+    color_dict = get_base_colors(model_and_search_names)
+    f_maj = plt.figure(figsize=(12, 6), dpi=300)
+    f_bon = plt.figure(figsize=(12, 6), dpi=300)
+    f_wmaj = plt.figure(figsize=(12, 6), dpi=300)
+    f_pan = plt.figure(figsize=(12, 6), dpi=300)
+    f_tok = plt.figure(figsize=(12, 6), dpi=300)
+
+    for (model, file), model_data in data.items():
+        fig_maj, fig_bon, fig_wmaj, fig_pan, fig_tok = plot_method(model_data, model, file, f_maj, f_bon, f_wmaj, f_pan, f_tok, color_dict, suffix_to_plot=suffix_to_plot)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    f_maj.savefig(f"{save_path}/majority_vote.png", bbox_inches='tight')
+    f_bon.savefig(f"{save_path}/best_of_n.png", bbox_inches='tight')
+    f_wmaj.savefig(f"{save_path}/weighted_majority_vote.png", bbox_inches='tight')
+    f_pan.savefig(f"{save_path}/pass_at_n.png", bbox_inches='tight')
+    f_tok.savefig(f"{save_path}/tokens.png", bbox_inches='tight')
+
+    plot_best_comparison(data, model_names, save_path, color_dict)
+
+def plot_best_comparison(data, model_names, save_path, color_dict):
+    fig = plt.figure(figsize=(12, 6), dpi=300)
+    for (model, file) in data.keys():
+        method_data = data[(model, file)]
+        search_method = file.split(".")[0].split("_")[0]
+        model_and_search_name = "_".join([model, search_method])
+
+        method_data["suffix"] = method_data["metric"].apply(lambda x: x.split("_")[-1] if "_" in x else "")
+        
+        # Get unique base method names
+        base_methods = sorted(set(m.split("_sum")[0].split("_min")[0].split("_last")[0] for m in method_data["metric"]))
+        base_methods.remove("time")
+
+        # Best method has the highest value
+        possible_methods = ["majority_vote", "best_of_n_sum", "best_of_n_min", "best_of_n_last","weighted_majority_vote_sum", "weighted_majority_vote_min", "weighted_majority_vote_last"]
+
+        metric_data = method_data[method_data["metric"].isin(possible_methods)]
+        best_row_idx = metric_data.idxmax()["value"]
+        best_row = metric_data.loc[best_row_idx]
+        best_method = best_row["metric"]
+        adv = best_row["adv"]
+        if adv:
+            model_and_search_name += "_adv"
+
+        color = color_dict[model_and_search_name]
+
+        sub_data = metric_data[metric_data["metric"] == best_method][metric_data["adv"] == adv]
+        ax = fig.gca()
+        sub_data = sub_data.sort_values("true_k")
+        ax.errorbar(
+            sub_data["true_k"],
+            sub_data["value"],
+            yerr=sub_data["std"],
+            label=f"{model_and_search_name} ({best_method})",
+            linestyle="solid",
+            color=color,
+            marker="o",
+            capsize=3
+        )
+
+    ax.legend()  # Ensure legend is added if any labels exist
+    ax.set_title("Best Comparison")
+    ax.set_xlabel("k")
+    ax.set_ylabel("Value")
+    ax.set_xscale("log", base=2)
+    ax.grid(True)
+    fig.savefig(f"{save_path}/best_comparison.png", bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    # model_names = ["qwen_genPPO_0.1" , "qwen_genPPO_0.3", "qwen_genPPO_0.5"]
+    model_names = ["qwen_genPPO_0.1"]
+    data = load_csvs(model_names)
+    # plot_all(data, model_names, save_path="plots/overall", suffix_to_plot=["last"])
+    plot_all(data, model_names, save_path="plots/qwen_genPPO_0.1", suffix_to_plot=["sum", "min", "last"])
